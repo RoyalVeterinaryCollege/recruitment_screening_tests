58229c490fec673ebf083b33ad493d61d6811e9a
Noel <nkennedy@rvc.ac.uk>
2020-01-29 14:04:21 +0000

    tidy

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,11 +10,12 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             //Feel free to run this code, but please don't change it
-           // Implementation.CountOfCode1(DataSource.GetSomeUniqueAnimals().ToList());
+            Implementation.CountOfCode1(DataSource.GetSomeUniqueAnimals().ToList());
             Implementation.ReturnUniqueCodes(DataSource.CreateSomeCodes(30).ToList());
             Implementation.AnimalsInBothLists(DataSource.GetSomeUniqueAnimals().ToList(), DataSource.GetSomeUniqueAnimals().ToList());
             Implementation.AnimalsOnlyInA(DataSource.GetSomeUniqueAnimals().ToList(), DataSource.GetSomeUniqueAnimals().ToList());
             Implementation.CountOfCode1And2(DataSource.GetSomeUniqueAnimals().ToList());
+            Console.WriteLine("Program completed.  Press any key.");
             Console.ReadLine();
         }
     }
